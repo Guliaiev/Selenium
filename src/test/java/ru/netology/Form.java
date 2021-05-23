b6258d71c1d4718c5c2dbfeae77d012525982202
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-05-23 20:18:18 +0300

    fix test

--- src/test/java/ru/netology/Form.java
+++ src/test/java/ru/netology/Form.java
@@ -1,92 +1,60 @@
 package ru.netology;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterEach;
+
+
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Form {
-    private WebDriver driver;
-    //private static ChromeOptions options;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-        //driver = new ChromeDriver(options);
-       // System.setProperty("webdriver.chrome.driver", "./webdriver/win/chromedriver.exe");
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--no-sandbox");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
+    @Test
+    void shouldInputIsCurrent() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Евгений Петров");
+        $("[data-test-id='phone'] input").setValue("+79787654321");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void testAllInputPositive() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Домен Моряк");
-        elements.get(1).sendKeys("+79270000022");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-
+    void shouldInputNameNotCurrent() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Evgeniy Petrov");
+        $("[data-test-id='phone'] input").setValue("+79787654321");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
-    void testNegativePhoneEmpty() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Дмитрий Евдокимов");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=phone] span.input__sub")).getText();
-        assertEquals("Поле обязательно для заполнения", text.trim());
+    void shouldInputPhoneNotCorrect() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Евгений Петров");
+        $("[data-test-id='phone'] input").setValue("89787654321");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void testNameEmpty() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(1).sendKeys("+79270000022");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
-        assertEquals("Поле обязательно для заполнения", text.trim());
+    void shouldChecboxNotClick() {
+        open("http://localhost:9999");
+        $("[data-test-id='name'] input").setValue("Евгений Петров");
+        $("[data-test-id='phone'] input").setValue("+79787654321");
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=agreement]").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".input_invalid[data-test-id=agreement]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
-    void testAgreementEmpty() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Домен Моряк");
-        elements.get(1).sendKeys("+79270000022");
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).getText();
-        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    void shouldNameNotInput() {
+        open("http://localhost:9999");
+        $("[data-test-id='phone'] input").setValue("+79787654321");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
     }
+
+
 }
